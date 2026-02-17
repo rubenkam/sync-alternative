b289f74dc7550a18d446160aba77a4bdd25cda68
rubenkam <rubenkamstra@gmail.com>
2026-02-17 23:04:48 +0100

    Dag release v1.0.0

--- dags/test.py
+++ dags/test.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
-
+# Trigger
 with DAG(
     dag_id="single_echo_dag",
     start_date=datetime(2024, 1, 1),
